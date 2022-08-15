25591fad6d09d39b6ba6ee16ba6a0dcc3c5a7bc2
hahwul <hahwul@gmail.com>
2022-08-15 22:35:02 +0900

    y

--- scripts/migration.rb
+++ scripts/migration.rb
@@ -5,17 +5,16 @@ data_hash = JSON.parse(file)
 
 data_hash.each do | name, obj |
      puts "filename: "+name+".yaml"
-     puts obj['Description']
 
      # Make object
      obj = {}
      obj['name'] = name
      obj['description'] = obj['Description']
      obj['url'] = '' # parse DATA
-     obj['categories'] = []
+     obj['category'] = 'tool'
      obj['types'] = []
      obj['lang'] = [] # parse DATA
      obj['tags'] = []
-
+     
      # Save file
 end
