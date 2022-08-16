ace7389b70e0d8ecb38643a4d766cc771bdb6344
hahwul <hahwul@gmail.com>
2022-08-16 20:48:09 +0900

    y

--- scripts/erb.rb
+++ scripts/erb.rb
@@ -5,42 +5,48 @@ template = %q{
 <br>
 <a href=""><img src="https://user-images.githubusercontent.com/13212227/104400969-9f3d9280-5596-11eb-80f4-864effae95fc.png" alt="" width="500px;"></a>
 <br>
-<img src="https://img.shields.io/github/languages/top/hahwul/WebHackersWeapons?style=flat"> 
 <img src="https://img.shields.io/github/last-commit/hahwul/WebHackersWeapons?style=flat"> 
 <img src="https://img.shields.io/badge/PRs-welcome-cyan">
 <img src="https://github.com/hahwul/WebHackersWeapons/workflows/Build/badge.svg">
-<img src="https://github.com/hahwul/WebHackersWeapons/workflows/CodeQL/badge.svg">
 <a href="https://twitter.com/intent/follow?screen_name=hahwul"><img src="https://img.shields.io/twitter/follow/hahwul?style=flat&logo=twitter"></a>
 </h1>
-A collection of cool tools used by Web hackers. Happy hacking , Happy bug-hunting
+A collection of awesome tools used by Web hackers. Happy hacking , Happy bug-hunting
 
 ## Family project
 [![WebHackersWeapons](https://img.shields.io/github/stars/hahwul/WebHackersWeapons?label=WebHackersWeapons)](https://github.com/hahwul/WebHackersWeapons)
 [![MobileHackersWeapons](https://img.shields.io/github/stars/hahwul/MobileHackersWeapons?label=MobileHackersWeapons)](https://github.com/hahwul/MobileHackersWeapons)
 
 ## Table of Contents
-- [WHW-Tools](https://whw-tools.hahwul.com)
 - [Weapons](#weapons)
-- [Awesome Bookmarklets](https://github.com/hahwul/WebHackersWeapons/tree/master/Bookmarklets)
-- [Awesome Browser Extensions](https://github.com/hahwul/WebHackersWeapons/tree/master/Browser%20Extensions)
-- [Awesome Burp and ZAP Extensions](https://github.com/hahwul/WebHackersWeapons/tree/master/Burp%20and%20ZAP%20Extensions)
+    - Tools
+    - [Bookmarklets](https://github.com/hahwul/WebHackersWeapons/tree/master/Bookmarklets)
+    - [Browser Extensions](https://github.com/hahwul/WebHackersWeapons/tree/master/Browser%20Extensions)
+    - [Burp and ZAP Extensions](https://github.com/hahwul/WebHackersWeapons/tree/master/Burp%20and%20ZAP%20Extensions)
 - [Contribute](https://github.com/hahwul/WebHackersWeapons/blob/master/CONTRIBUTING.md) 
 - [Thanks to contributor](#thanks-to-contributor)
 
-## WHW-Tools
-> Always use the latest tools 😎
-
-`WHW-Tools` is tools web of `#WebHackersWeapons`. Easy install and Easy manage upgrade. Go to [WHW-Tools](https://whw-tools.hahwul.com/)
-
 ## Weapons
-<%= aa %>
-  
+### Tools
+<%= tools %>
+
+### Bookmarklets
+<%= bookmarklets %>
+
+### Browser Addons
+<%= browser_addons %>
+
+### Burpsuite and ZAP Addons
+<%= burpzap_addons %>
+
 ## Thanks to (Contributor)
 I would like to thank everyone who helped with this project 👍😎 
 ![](/CONTRIBUTORS.svg)
 
 }.gsub(/^  /, '')
-aa = 4414
+tools = 4414
+bookmarklets = 111
+browser_addons = 111
+burpzap_addons = 111
 
 markdown = ERB.new(template, trim_mode: "%<>")
 puts markdown.result
